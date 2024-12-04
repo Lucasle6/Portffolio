a54b95df3039cd11f7882ed5601d58a479a666d8
FarahHadid <j.cleon695@gmail.com>
2024-12-04 02:33:00 +0100

    first steps, adding navbar and logos for socials (linkedin and github)

--- my_portfolio/src/App.jsx
+++ my_portfolio/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline text-white'>
-        Hello World!
-      </h1>
-      
+    <div className='overflow-x-hidden text-stone-300 antialiased'>
+      <div className='fixed inset-0 -z-10'>
+        <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      </div>
+      <div className='container mx-auto px-8'>
+        <Navbar/>
+      </div>
     </div>
   )
 }
