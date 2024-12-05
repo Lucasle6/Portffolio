7b68a186266ec32b514fe9c38f320d971ed14d8a
FarahHadid <j.cleon695@gmail.com>
2024-12-05 13:51:00 +0100

    Fixing animation in Technologies component to have the final x position set to 0 and centered in the device

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -26,56 +26,56 @@ const Technologies = () => {
     <div className='pb-24'>
         <motion.h2 
             className='my-20 text-center text-4xl'
-            whileInView={{opacity: 1, y: 0}}
+            whileInView={{opacity: 1, x: 0}}
             initial={{opacity: 0, x: -100}}
             transition={{duration: 1.5}}>
                 Technologies
         </motion.h2>
         <motion.div 
-            className='flex flex-wrap items-center justify-center gap-4'
-            whileInView={{opacity: 1, y: 0}}
-            initial={{opacity: 0, x: -100}}
+            className='flex flex-wrap justify-center items-center gap-4'
+            whileInView={{opacity: 1, x: 0}}
+            initial={{opacity: 0, x:-100}}
             transition={{duration: 1.5}}>
             <motion.div
                 initial= "initial"
                 animate="animate"
                 variants={iconVariants(2.5)}>
-                <RiReactjsLine className='text-7xl text-cyan-400'/>
+                    <RiReactjsLine className='text-7xl text-cyan-400'/>
             </motion.div>
             <motion.div
-            initial= "initial"
-            animate="animate"
-            variants={iconVariants(3)}
-            className='p-4'>
-                <TbBrandNextjs className='text-7xl'/>
+                initial= "initial"
+                animate="animate"
+                variants={iconVariants(3)}
+                className='p-4'>
+                    <TbBrandNextjs className='text-7xl'/>
             </motion.div>
             <motion.div
-            initial= "initial"
-            animate="animate"
-            variants={iconVariants(5)}
-            className='p-4'>
-                <SiJavascript className='text-7xl text-yellow-500'/>
+                initial= "initial"
+                animate="animate"
+                variants={iconVariants(5)}
+                className='p-4'>
+                    <SiJavascript className='text-7xl text-yellow-500'/>
             </motion.div>
             <motion.div
-            initial= "initial"
-            animate="animate"
-            variants={iconVariants(2)}
-            className='p-4'>
-                <FaCss3 className='text-7xl text-blue-500'/>
+                initial= "initial"
+                animate="animate"
+                variants={iconVariants(2)}
+                className='p-4'>
+                    <FaCss3 className='text-7xl text-blue-500'/>
             </motion.div>
             <motion.div 
-            initial= "initial"
-            animate="animate"
-            variants={iconVariants(6)}
-            className='p-4'>
-                <FaNodeJs className='text-7xl text-green-500'/>
+                initial= "initial"
+                animate="animate"
+                variants={iconVariants(6)}
+                className='p-4'>
+                    <FaNodeJs className='text-7xl text-green-500'/>
             </motion.div>            
             <motion.div 
-            initial= "initial"
-            animate="animate"
-            variants={iconVariants(4)}
-            className='p-4'>
-                <RiTailwindCssFill className='text-7xl text-cyan-600'/>
+                initial= "initial"
+                animate="animate"
+                variants={iconVariants(4)}
+                className='p-4'>
+                    <RiTailwindCssFill className='text-7xl text-cyan-600'/>
             </motion.div>
         </motion.div>
     </div>
